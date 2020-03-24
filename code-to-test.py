61cc566f99a7c7803672dc1144705e1654c58fd2
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-03-24 20:10:44 -0300

    added append and loop

--- code-to-test.py
+++ code-to-test.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
 populationData = pd.read_json('https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-population.json')
-
+stringDataFrame = pd.DataFrame(pd.np.empty(0,1))
 for index, row in populationData.iterrows():
-    print(row[0] + ' has population ' + str(row[1]))
+    stringDataFrame.append(pd.Series(row[0] + ' has population ' + str(row[1]))
+
+i=0
+while i < 10**8:
+    i+=1
+
+print('The code is done!')

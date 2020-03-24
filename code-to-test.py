353edfd22c3c1f7cec32b22d9c037f97e4494010
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-03-24 19:43:26 -0300

    fixed integer error

--- code-to-test.py
+++ code-to-test.py
@@ -3,4 +3,4 @@ import pandas as pd
 populationData = pd.read_json('https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-population.json')
 
 for dataPoint in populationData:
-  print(dataPoint['country'] + ' has population ' + dataPoint[''])
+  print(dataPoint['country'] + ' has population ' + str(dataPoint['population']))
